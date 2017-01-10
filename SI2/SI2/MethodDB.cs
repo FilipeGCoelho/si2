e97138c5aabe8b2f680c72a28525edc5c610ab9a
Rui Franco <36210@alunos.isel.ipl.pt>
2017-01-10 17:27:07 +0000

    .

---
+++ SI2/SI2/MethodDB.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace SI2
+{
+    class MethodDB
+    {
+        private static readonly string ConnectionString = "user id=si2;password=si2;database=SI2;";
+        private static readonly SqlConnection Connection = new SqlConnection(ConnectionString);
+
+        public static void Init()
+        {
+            try
+            {
+                Connection.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        //Inserir, remover e actualizar informação de promoções
+
+        public static void e_ef()
+        {
+
+        }
+        public static void e_ado_net()
+        {
+
+        }
+
+        public static void e_ado_insert()
+        {
+            SqlCommand command = new SqlCommand("Insert_Promocao", Connection);
+            command.CommandType = CommandType.StoredProcedure;
+            DateTime bixo = new DateTime(1999, 99, 99);
+            //          @dataInicio DATE,
+            //	        @dataFim DATE,
+            //	        @descricao VARCHAR(128),
+            //	        @tipo VARCHAR(8)
+
+            SqlParameter dataInicio = new SqlParameter("@dataInicio", SqlDbType.Date);
+            dataInicio.Value = bixo;
+            command.Parameters.Add(dataInicio);
+            SqlParameter dataFim = new SqlParameter("@dataFim", SqlDbType.Date);
+            SqlParameter descricao = new SqlParameter("@descricao", SqlDbType.VarChar);
+            SqlParameter tipo = new SqlParameter("@tipo", SqlDbType.VarChar);
+        }
+
+        public static void e_ado_remove()
+        {
+            new SqlCommand("Remove_Promocao", Connection).ExecuteNonQuery();
+        }
+
+        public static void e_ado_update()
+        {
+            new SqlCommand("Update_Promocao", Connection).ExecuteNonQuery();
+        }
+
+        //Inserir um aluguer com inserção de um cliente com dados completos
+        public static void f_ef()
+        {
+
+        }
+
+        public static void f_ado_net()
+        {
+
+        }
+
+        //Inserir um aluguer usando um cliente existente;
+        public static void g_ef()
+        {
+
+        }
+
+        public static void g_ado_net()
+        {
+
+        }
+
+        //Remover Aluguer
+        public static void h_ef()
+        {
+
+        }
+
+        public static void h_ado_net()
+        {
+
+        }
+
+        //Efectuar alterações de preçário
+        public static void i_ef()
+        {
+
+        }
+
+        public static void i_ado_net()
+        {
+
+        }
+
+        //Listar todos os equipamentos livres, para um determinado tempo e tipo
+        public static void j_ef()
+        {
+
+        }
+
+        public static void j_ado_net()
+        {
+
+        }
+
+        //Listar todos os equipamentos livres, para um determinado tempo e tipo
+        public static void k_ef()
+        {
+
+        }
+
+        public static void k_ado_net()
+        {
+
+        }
+    }
+
+
+}
+
--- SI2/SI2/Program.cs
+++ SI2/SI2/Program.cs
@@ -1,13 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Net.NetworkInformation;
-using System.Reflection;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace SI2
 {
@@ -54,7 +47,7 @@ namespace SI2
             //check if answer is valid
             OperationInfo opi;
 
-            if(operations.TryGetValue(answer_command,out opi))
+            if (operations.TryGetValue(answer_command, out opi))
             {
                 //choose the pretended mode (EF or ADO)
                 Console.Clear();
@@ -98,30 +91,40 @@ namespace SI2
             Thread.Sleep(1500);
             Environment.Exit(0);
         }
-          
+
         public static void Init()
         {
-            operations.Add("e1", new OperationInfo("Inserir informação de promoções", MethodDB.e1_ef, MethodDB.e1_ado_net));
-            operations.Add("e2", new OperationInfo("Remover informação de promoções", MethodDB.e2_ef, MethodDB.e2_ado_net));
-            operations.Add("e3", new OperationInfo("Actualizar informação de promoções", MethodDB.e3_ef, MethodDB.e3_ado_net));
+            operations.Add("e1",
+                new OperationInfo("Inserir informação de promoções", MethodDB.e_ef, MethodDB.e_ado_insert));
+            operations.Add("e2",
+                new OperationInfo("Remover informação de promoções", MethodDB.e_ef, MethodDB.e_ado_remove));
+            operations.Add("e3",
+                new OperationInfo("Actualizar informação de promoções", MethodDB.e_ef, MethodDB.e_ado_update));
 
-            operations.Add("f", new OperationInfo("Inserir um aluguer com inserção de um cliente com dados completos", MethodDB.f_ef, MethodDB.f_ado_net));
+            operations.Add("f",
+                new OperationInfo("Inserir um aluguer com inserção de um cliente com dados completos", MethodDB.f_ef,
+                    MethodDB.f_ado_net));
 
-            operations.Add("g", new OperationInfo("Inserir um aluguer usando um cliente existente", MethodDB.g_ef, MethodDB.g_ado_net));
+            operations.Add("g",
+                new OperationInfo("Inserir um aluguer usando um cliente existente", MethodDB.g_ef, MethodDB.g_ado_net));
 
-            operations.Add("h", new OperationInfo("Remover um Aluguer",MethodDB.h_ef,MethodDB.h_ado_net));
+            operations.Add("h", new OperationInfo("Remover um Aluguer", MethodDB.h_ef, MethodDB.h_ado_net));
 
             operations.Add("i", new OperationInfo("Efectuar alterações de preçário", MethodDB.i_ef, MethodDB.i_ado_net));
 
-            operations.Add("j", new OperationInfo("Listar todos os equipamentos livres, para um determinado tempo e tipo", MethodDB.j_ef, MethodDB.j_ado_net));
+            operations.Add("j",
+                new OperationInfo("Listar todos os equipamentos livres, para um determinado tempo e tipo", MethodDB.j_ef,
+                    MethodDB.j_ado_net));
 
-            operations.Add("k", new OperationInfo("Listar os equipamentos sem alugueres na última semana", MethodDB.k_ef, MethodDB.k_ado_net));
+            operations.Add("k",
+                new OperationInfo("Listar os equipamentos sem alugueres na última semana", MethodDB.k_ef,
+                    MethodDB.k_ado_net));
         }
     }
 
 
     public delegate void MethodDelegate();
-    
+
     class OperationInfo
     {
         public string phrase;
@@ -135,115 +138,5 @@ namespace SI2
             method_ADO_NET = ado;
         }
     }
-
-    class MethodDB
-    {
-        private static readonly string ConnectionString = "user id=si2;password=si2;database=SI2;";
-        private static readonly SqlConnection Connection = new SqlConnection(ConnectionString);
-
-        public static void Init()
-        {
-            try
-            {
-                Connection.Open();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-        }
-
-        //Inserir, remover e actualizar informação de promoções
-        public static void e_ado_net()
-
-        public static void e_ado_insert()
-            SqlCommand command = new SqlCommand("Insert_Promocao", Connection);
-            command.CommandType = CommandType.StoredProcedure;
-            DateTime bixo= new DateTime(1999, 99, 99);
-//          @dataInicio DATE,
-//	        @dataFim DATE,
-//	        @descricao VARCHAR(128),
-//	        @tipo VARCHAR(8)
-
-            SqlParameter dataInicio = new SqlParameter("@dataInicio", SqlDbType.Date);
-            dataInicio.Value = bixo;
-            command.Parameters.Add(dataInicio);
-            SqlParameter dataFim = new SqlParameter("@dataFim", SqlDbType.Date);
-            SqlParameter descricao = new SqlParameter("@descricao", SqlDbType.VarChar);
-            SqlParameter tipo = new SqlParameter("@tipo", SqlDbType.VarChar);
-        public static void e_ado_remove()
-            new SqlCommand("Remove_Promocao", Connection).ExecuteNonQuery();                        
-        public static void e_ado_update()
-        {
-            new SqlCommand("Update_Promocao", Connection).ExecuteNonQuery();                        
-        }
-
-        //Inserir um aluguer com inserção de um cliente com dados completos
-        public static void f_ef()
-        {
-
-        }
-
-        public static void f_ado_net()
-        {
-
-        }
-
-        //Inserir um aluguer usando um cliente existente;
-        public static void g_ef()
-        {
-
-        }
-
-        public static void g_ado_net()
-        {
-
-        }
-
-        //Remover Aluguer
-        public static void h_ef()
-        {
-            
-        }
-
-        public static void h_ado_net()
-        {
-
-        }
-
-        //Efectuar alterações de preçário
-        public static void i_ef()
-        {
-
-        }
-
-        public static void i_ado_net()
-        {
-
-        }
-
-        //Listar todos os equipamentos livres, para um determinado tempo e tipo
-        public static void j_ef()
-        {
-
-        }
-
-        public static void j_ado_net()
-        {
-
-        }
-
-        //Listar todos os equipamentos livres, para um determinado tempo e tipo
-        public static void k_ef()
-        {
-
-        }
-
-        public static void k_ado_net()
-        {
-
-        }
-    }
-
 
 }
