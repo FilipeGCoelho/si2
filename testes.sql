a9bcdabb2ba9e032d5d2ccd80790d813e73eb3b1
Rui Franco <36210@alunos.isel.ipl.pt>
2017-01-12 20:36:32 +0000

    porra

--- testes.sql
+++ testes.sql
@@ -54,7 +54,7 @@ select * from Promocao
 -- update
 
 select * from Promocao
-exec Update_Promocao 1, '', null, 'descPromo ', null
+exec Update_Promocao 1, '2016-01-01' , '2018-01-01', 'descPromo teste', 'desconto'
 select * from Promocao
 
 -- remove
